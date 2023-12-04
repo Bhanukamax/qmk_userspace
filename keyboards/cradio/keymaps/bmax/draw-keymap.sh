70b358155633d35f2add228cc346f679bf145ee5
Don Bhanuka <bhanukamax@gmail.com>
2023-12-04 07:42:28 +0530

    Added keymap drawer layer names

--- keyboards/cradio/keymaps/bmax/draw-keymap.sh
+++ keyboards/cradio/keymaps/bmax/draw-keymap.sh
@@ -1,3 +1,4 @@
 L_NAMES="_DVORAK _APTMAK _NAV NUM FN MUS GAME SYS SYM1 NUM1 SNUM1"
-qmk c2json ~/qmk_userspace/keyboards/cradio/keymaps/bmax/keymap.c | keymap parse --layer-names $(echo $L_NAMES) -c 10 -q - >sweep_keymap.yaml
-keymap draw sweep_keymap.yaml >sweep.svg
+#qmk c2json ~/qmk_userspace/keyboards/cradio/keymaps/bmax/keymap.c | keymap parse --layer-names $(echo $L_NAMES) -c 10 -q - >sweep_keymap.yaml
+qmk c2json ~/qmk_userspace/keyboards/cradio/keymaps/bmax/keymap.c | keymap -c my_config.yaml parse --layer-names $(echo $L_NAMES) -c 10 -q - >sweep_keymap.yaml
+keymap -c my_config.yaml draw sweep_keymap.yaml >sweep.svg
